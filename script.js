3984187ef573d1cd44bbfbfc35cb799e8fb14152
Andy Lulham <a.lulham@gmail.com>
2014-03-19 01:23:14 +0000

    Copy updates

--- script.js
+++ script.js
@@ -31,11 +31,11 @@ function startGame() {
       a: [
         {
           text: 'Under 25',
-          feedback: 'Ah. <a target="_blank" href="http://www.theguardian.com/business/2014/feb/05/unemployment-statistics-age-divide-recession">Almost 20% of under 25s are unemployed</a>.<br><img src="i/job_centre.jpg" />',
+          feedback: 'Ah. Unlucky for you, <a target="_blank" href="http://www.theguardian.com/business/2014/feb/05/unemployment-statistics-age-divide-recession">almost 20% of under 25s are unemployed</a>.<br><img src="i/job_centre.jpg" />',
           score: -2
         }, {
           text: 'Over 25',
-          feedback: 'Great! Pity about family / friends / <a target="_blank" href="http://www.theguardian.com/business/2014/feb/05/unemployment-statistics-age-divide-recession">anyone you know under 25</a>…<br><img src="i/job_centre.jpg" />',
+          feedback: 'Well at least you’re doing better than <a target="_blank" href="http://www.theguardian.com/business/2014/feb/05/unemployment-statistics-age-divide-recession">any friends or family members under 25</a>…<br><img src="i/job_centre.jpg" />',
           score: 0
         }
       ]
@@ -44,11 +44,11 @@ function startGame() {
       a: [
         {
           text: 'Yes',
-          feedback: 'What a relief! Out of every 100 private sector jobs created, <a target="_blank" href="http://www.theguardian.com/business/2014/feb/05/unemployment-statistics-age-divide-recession">only 37 are going to women</a>.',
+          feedback: 'What a relief! Out of every 100 private sector jobs created, <a target="_blank" href="http://www.theguardian.com/society/2013/sep/21/spending-cuts-women-report">only 37 are going to women</a>.',
           score: 0
         }, {
           text: 'No',
-          feedback: 'What a shame. <a target="_blank" href="http://www.theguardian.com/business/2013/jul/01/public-sector-austerity-measures-women">Women aren’t faring so well under austerity measures</a>. And it’s <a target="_blank" href="http://www.independent.co.uk/news/uk/politics/exclusive-george-osbornes-tax-and-benefits-changes-hit-women-almost-four-times-harder-than-men-9177533.html">even worse if you’ve got children</a>.',
+          feedback: 'What a shame. <a target="_blank" href="http://www.theguardian.com/business/2013/jul/01/public-sector-austerity-measures-women">Women aren’t doing so well under austerity measures</a>. And out of every 100 private sector jobs created, <a target="_blank" href="http://www.theguardian.com/society/2013/sep/21/spending-cuts-women-report">only 37 are going to women</a>.',
           score: -1
         }
       ]
@@ -57,40 +57,40 @@ function startGame() {
       a: [
         {
           text: 'Chelsea',
-          feedback: '<img src="i/kensington.jpg" /><br>Fancy buying <a target="_blank" href="http://www.telegraph.co.uk/property/propertynews/10700500/One-London-home-or-25-in-Scotland.html">25 houses in Scotland?</a>',
+          feedback: 'Oh that’s lovely for you. <a target="_blank" href="http://www.zoopla.co.uk/home-values/browse/london/sw3/chelsea-brompton/?q=SW3&search_source=home-values">The average property in Chelsea is worth almost £4 million.</a>',
           score: 1
         }, {
           text: 'Westminster',
-          feedback: '<img src="i/kensington.jpg" /><br>Fancy buying <a target="_blank" href="http://www.telegraph.co.uk/property/propertynews/10700500/One-London-home-or-25-in-Scotland.html">25 houses in Scotland?</a>',
+          feedback: 'Oh that’s lovely for you. <a target="_blank" href="http://www.zoopla.co.uk/home-values/london/whitehall/">The average property in Westminster is worth almost £6.5 million.</a>',
           score: 1
         }, {
           text: 'Anywhere else',
-          feedback: 'Uh oh! Won’t be long before an oligarch <a target="_blank" href="http://www.telegraph.co.uk/property/propertynews/10700500/One-London-home-or-25-in-Scotland.html">buys up your whole street</a>.',
+          feedback: 'Oh dear… your house won’t be worth quite as much as in <a target="_blank" href="http://www.zoopla.co.uk/home-values/browse/london/sw3/chelsea-brompton/?q=SW3&search_source=home-values">Chelsea (average £4 million)</a> or <a target="_blank" href="http://www.zoopla.co.uk/home-values/london/whitehall/">Westminster (average £6.5 million)</a> then…',
           score: -1
         }
       ]
     }, {
-      q: 'What’s your annual income?',
+      q: 'How much do you earn?',
       a: [
         {
           text: 'Roughly &nbsp;&nbsp;£1.2bn&nbsp;&nbsp;',
-          feedback: 'Sounds like your finances will soon be bouncing back!<br><img src="i/green.jpg" /><br>Funnily enough, that’s what <a target="_blank" href="http://www.ukuncut.org.uk/targets/3">tax-avoiding topshop owner Sir Philip Green earned in 2005</a>.',
+          feedback: 'Sounds like your finances will soon be bouncing back!<br><img src="i/green.jpg" /><br>Funnily enough, that’s what <a target="_blank" href="http://www.ukuncut.org.uk/targets/3">tax-avoiding topshop owner Sir Philip Green earned in 2005!</a>',
           score: 5
         }, {
           text: 'Around £142,500',
-          feedback: 'Not bad – that’s what <a target="_blank" href="http://www.mirror.co.uk/news/uk-news/uk-average-salary-26500-figures-3002995">the prime minister took home last year</a>.',
+          feedback: 'Not bad – that’s what <a target="_blank" href="http://www.mirror.co.uk/news/uk-news/uk-average-salary-26500-figures-3002995">the Prime Minister took home last year</a>. But still almost 9000 times less than <a target="_blank" href="http://www.ukuncut.org.uk/targets/3">the £1.2 billion tax-avoiding Topshop owner Sir Philip Green earned in 2005</a>.<br><img src="i/green.jpg" />',
           score: 1
         }, {
           text: 'Close to £39,346',
-          feedback: 'That’s the <a target="_blank" href="http://www.mirror.co.uk/news/uk-news/uk-average-salary-26500-figures-3002995">average salary of a police officer</a>.',
+          feedback: 'That’s the <a target="_blank" href="http://www.mirror.co.uk/news/uk-news/uk-average-salary-26500-figures-3002995">average salary of a police officer</a>. But over 30,000 times less than <a target="_blank" href="http://www.ukuncut.org.uk/targets/3">the £1.2 billion tax-avoiding Topshop owner Sir Philip Green earned in 2005</a>.<br><img src="i/green.jpg" />',
           score: -1
         }, {
           text: 'Closer to £26,500',
-          feedback: 'Oh dear… Well you’re certainly not alone in <a target="_blank" href="http://www.mirror.co.uk/news/uk-news/uk-average-salary-26500-figures-3002995">earning close to the UK average wage</a>, if that’s any consolation.',
+          feedback: 'Hmm… That’s the <a target="_blank" href="http://www.mirror.co.uk/news/uk-news/uk-average-salary-26500-figures-3002995">average annual salary</a>. But almost 50,000 times less than <a target="_blank" href="http://www.ukuncut.org.uk/targets/3">the £1.2 billion tax-avoiding Topshop owner Sir Philip Green earned in 2005</a>.<br><img src="i/green.jpg" />',
           score: -1
         }, {
-          text: 'More like £12,873',
-          feedback: 'That’s the <a target="_blank" href="http://www.mirror.co.uk/news/uk-news/uk-average-salary-26500-figures-3002995">Minimum Wage as an annual salary</a>.',
+          text: 'More like £13,124',
+          feedback: 'Sorry to hear that. <a target="_blank" href="http://www.mirror.co.uk/news/uk-news/uk-average-salary-26500-figures-3002995">That’s the Minimum Wage as an annual salary.</a> That’s almost 100,000 times less than <a target="_blank" href="http://www.ukuncut.org.uk/targets/3">the £1.2 billion tax-avoiding Topshop owner Sir Philip Green earned in 2005</a>.<br><img src="i/green.jpg" />',
           score: -2
         }
       ]
@@ -99,15 +99,11 @@ function startGame() {
       a: [
         {
           text: 'Eton',
-          feedback: 'Excellent! Hopefully that means you’re up there with the 1%!',
-          score: 0
-        }, {
-          text: 'Harrow',
-          feedback: 'Excellent! Hopefully that means you’re up there with the 1%!',
+          feedback: 'Good for you old chap. Although even <a target="_blank" href="http://www.theguardian.com/politics/2014/mar/15/michael-gove-old-etonians-conservative-david-cameron">Education Secretary Michael Gove seems to think it’s “ridiculous” and “preposterous” that your old classmates are still running the show.</a>',
           score: 0
         }, {
           text: 'Other',
-          feedback: 'Ouch! Even Education Secretary Michael Gove admits the number of Old Etonians in David Cameron’s inner circle is <a target="_blank" href="www.theguardian.com/politics/2014/mar/15/michael-gove-old-etonians-conservative-david-cameron">“ridiculous” and “preposterous”</a>.',
+          feedback: 'One of those other schools? Well even <a target="_blank" href="http://www.theguardian.com/politics/2014/mar/15/michael-gove-old-etonians-conservative-david-cameron">Education Secretary Michael Gove admits the number of Old Etonians at the top of the government is “ridiculous” and “preposterous”</a>.',
           score: -1
         }
       ]
@@ -116,11 +112,11 @@ function startGame() {
       a: [
         {
           text: 'It could happen.',
-          feedback: 'Bad news for you. <a target="_blank" href="http://www.bbc.co.uk/news/business-22804563">600,000 could lose incapacity benefits</a>.',
+          feedback: 'Bad news for you. <a target="_blank" href="http://www.bbc.co.uk/news/uk-politics-25617844">George Osborne is planning to make another £25 billion worth of cuts to the welfare budget!</a>',
           score: -1
         }, {
           text: 'There is no possible chance of that.',
-          feedback: 'That’s the spirit! Best keep it that way, given <a target="_blank" href="http://www.bbc.co.uk/news/business-22804563">600,000 could lose incapacity benefits</a>.',
+          feedback: 'That’s the spirit! Best keep it that way <a target="_blank" href="http://www.bbc.co.uk/news/uk-politics-25617844">given George Osborne is planning to make another £25 billion worth of cuts to the welfare budget.</a>',
           score: 0
         }
       ]
